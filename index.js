e39b1aad66a47c5e4aef5687e0b2ae3c6d349571
Vineet <vineet.kmrp@outlook.com>
2025-05-02 18:19:58 +0530

    This is a new branch

--- index.js
+++ index.js
@@ -11,5 +11,8 @@ function above18(){
     return age>18
 }
 function newfn(){
-    console.log("Hey I am new fn")
+    console.log("Hey I am new fn")git 
+}
+function newfeatureadd(){
+    console.log("This is a new feature")
 }
